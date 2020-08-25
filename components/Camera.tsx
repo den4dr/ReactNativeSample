160f138040c72ddbc608f9ca25a0bec7634b20d4
Hayao S <main@den4dr.com>
2020-08-25 18:54:07 +0900

    Camera.tsxに着手

---
+++ components/Camera.tsx
@@ -0,0 +1,102 @@
+/**
+ * Camera.js
+ * Camera component
+ */
+
+import React from "react";
+import {Text, View, TouchableOpacity} from "react-native";
+import { Toast } from "native-base";
+import { Camera, Permissions, Icon} from "expo";
+
+import Colors from "../constants/Colors";
+
+export default class CameraComponent extends React.Component<Props, {}> {
+    private Camera = Camera;
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasCameraPermission: null,
+            type: Camera.Constants.Type.back,
+        };
+
+        this.takePicture = this.takePicture.bind(this);
+    }
+
+    // 初期起動時、カメラの使用の権限を取得する。
+    async componentWillMount() {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({
+            hasCameraPermission: status === 'granted',
+        });
+    }
+
+    // 撮影
+    async takePicture() {
+        const pictureData = await this.Camera.takePictureAsync();
+        Toast.show({
+            text: 'Success',
+            buttonText: 'Okay',
+            duration: 2000,
+        });
+        console.log(pictureData.uri);
+    }
+
+    render() {
+        const {
+            hasCameraPermission,
+        } = this.state;
+
+        if (hasCameraPermission === null || hasCameraPermission === false) {
+            return (
+                <View>
+                    <Text>
+                        カメラの使用が許可されていません。
+                    </Text>
+                </View>
+            );
+        }
+        return (
+            <View
+                style={{
+                    flex: 1,
+                }}
+            >
+                <Camera
+                    style={{
+                        flex: 1,
+                    }}
+                    type={this.state.type}
+                    ref={(ref) => {
+                        this.camera = ref;
+                    }}
+                >
+                    <View
+                        style={{
+                            flex: 1,
+                            backgroundColor: 'transparent',
+                            flexDirection: 'row',
+                        }}
+                    >
+                        <TouchableOpacity
+                            style={{
+                                flex: 1,
+                                alignSelf: 'flex-end',
+                                alignItems: 'center',
+                            }}
+                            onPress={() => {
+                                this.takePicture();
+                            }}
+                        >
+                            <Icon.MaterialIcons
+                                name="camera"
+                                size={70}
+                                style={{ marginBottom: 20 }}
+                                color={Colors.tabIconDefault}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </Camera>
+            </View>
+        );
+    }
+}
